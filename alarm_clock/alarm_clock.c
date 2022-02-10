97cf358395bd41675a51f0fe3ea129899fe819e3
Lars Tønder <lars.tonder@outlook.com>
2022-02-10 18:38:07 +0100

    get current datetime

--- alarm_clock/alarm_clock.c
+++ alarm_clock/alarm_clock.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
-    printf("Hello, World!");
+
+    time_t t;
+    time(&t);
+
+    printf("Welcome to the alarm clock! It is currently %s.", ctime(&t));
+    
     return 0;
 }
